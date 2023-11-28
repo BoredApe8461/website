d057e0b80063cc9abfc44cbeb422e6ed0fbfd14f
BoredApe8461 <letesinkelegesse@gmail.com>
2023-11-28 17:56:04 +0100

    responsive

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,47 +1,73 @@
-import React from 'react';
-import sergej from '../images/sergej.jpg';
-import oliver from '../images/oliver.jpg';
+import React from "react";
+import sergej from "../images/sergej.jpg";
+import oliver from "../images/oliver.jpg";
 
-const Team = () => {
+const Member = ({name, role, description, image}) => {
     return (
-        <div className="mt-8 bg-gray-100">
-            <section id="team" data-aos="fade-up">
-                <div className="my-4 py-4">
-                        <h2 className="my-2 text-center text-3xl text-gray-900 uppercase font-bold">Team</h2>
-                        <div className='flex justify-center'>
-                            <div className='w-24 border-b-4 border-blue-900'></div>
-                        </div>
-                    </div>
-
-                
-                <div className="p-16" data-aos="fade-in" data-aos-delay="600">
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                        <div style={{ textAlign: "center", width: "350px", marginRight: "150px" }}>
-                            <div style={{display: "flex", justifyContent: "center"}}>
-                                <img style={{ borderRadius: "100%", width: "250px" }} src={sergej} alt="Sergej Sakac" />
-                            </div>
-                            <h2 className="my-2 text-center text-gray-900 font-bold">Sergej Sakac</h2>
-                            <h3 className="my-2 text-center text-gray-900 italic">Core developer & Founder</h3>
-                            <p className="my-2 text-center text-gray-900">
-                                Sergej is a member of the Polkadot Fellowship. He has been an external core contributor on substrate and polkadot for more than a year now. Sergej is also a recent Engineering alumni of the Polkadot Blockchain Academy (PBA) held in Berkeley.
-                            </p>
-                        </div>
-
-                        <div style={{ textAlign: "center", width: "350px", marginLeft: "150px" }}>
-                            <div style={{display: "flex", justifyContent: "center"}}>
-                                <img style={{ borderRadius: "100%", width: "250px" }} src={oliver} alt="Oliver Lim" />
-                            </div>
-                            <h2 className="my-2 text-center text-gray-900 font-bold">Oliver Lim</h2>
-                            <h3 className="my-2 text-center text-gray-900 italic">Full-stack developer</h3>
-                            <p className="my-2 text-center text-gray-900">
-                                Oliver is a member of the Polkadot Fellowship. He has been an external core contributor on substrate and polkadot for more than a year now. Oliver is also a recent Engineering alumni of the Polkadot Blockchain Academy (PBA) held in Berkeley.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+        <div
+          style={{
+            textAlign: "center",
+            width: "350px",
+            marginLeft: "4em",
+            marginRight: "4em",
+            marginTop: "2em",
+          }}
+        >
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img
+              style={{ borderRadius: "100%", width: "250px" }}
+              src={image}
+              alt="Sergej Sakac"
+            />
+          </div>
+          <h2 className="my-2 text-center text-gray-900 font-bold">
+            {name}
+          </h2>
+          <h3 className="my-2 text-center text-gray-900 italic">
+            {role}
+          </h3>
+          <p className="my-2 text-center text-gray-900">
+            {description}
+          </p>
         </div>
     )
 }
+
+const teamStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginTop: "5em"
+}
+
+const Team = () => {
+  return (
+    <div className="mt-8 bg-gray-100">
+      <section id="team" data-aos="fade-up">
+        <div className="my-4 py-4">
+          <div className="my-4 py-4">
+            <h2 className="my-2 text-center text-3xl text-gray-900 uppercase font-bold">
+              Team
+            </h2>
+            <div className="flex justify-center">
+              <div className="w-24 border-b-4 border-blue-900"></div>
+            </div>
+          </div>
+
+          <div style={teamStyle} data-aos="fade-in" data-aos-delay="600">
+            <Member name="Sergej Sakac" role="Core developer & Founder" description={`Sergej is a member of the Polkadot Fellowship. He has been an
+                external core contributor on substrate and polkadot for more
+                than a year now. Sergej is also a recent Engineering alumni of
+                the Polkadot Blockchain Academy (PBA) held in Berkeley.`} image={sergej}/>
+            <Member name="Oliver Lim" role="Full-stack developer" description={`Oliver is a member of the Polkadot Fellowship. He has been an
+                external core contributor on substrate and polkadot for more
+                than a year now. Oliver is also a recent Engineering alumni of
+                the Polkadot Blockchain Academy (PBA) held in Berkeley.`} image={oliver}/>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Team;
