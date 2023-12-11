9454d437bc1dd7d14b5b9f6d1766f91e10846f38
BoredApe8461 <letesinkelegesse@gmail.com>
2023-12-11 05:38:57 -0800

    update team section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,43 +2,37 @@ import React from "react";
 import sergej from "../images/sergej.jpg";
 import oliver from "../images/oliver.jpg";
 
-const Member = ({name, role, description, image}) => {
-    return (
-        <div
-          style={{
-            textAlign: "center",
-            width: "350px",
-            marginLeft: "4em",
-            marginRight: "4em",
-            marginTop: "2em",
-          }}
-        >
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <img
-              style={{ borderRadius: "100%", width: "250px" }}
-              src={image}
-              alt="Sergej Sakac"
-            />
-          </div>
-          <h2 className="my-2 text-center text-gray-900 font-bold">
-            {name}
-          </h2>
-          <h3 className="my-2 text-center text-gray-900 italic">
-            {role}
-          </h3>
-          <p className="my-2 text-center text-gray-900">
-            {description}
-          </p>
-        </div>
-    )
-}
+const Member = ({ name, role, description, image }) => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        width: "350px",
+        marginLeft: "4em",
+        marginRight: "4em",
+        marginTop: "2em",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          style={{ borderRadius: "100%", width: "250px" }}
+          src={image}
+          alt="Sergej Sakac"
+        />
+      </div>
+      <h2 className="my-2 text-center text-gray-900 font-bold">{name}</h2>
+      <h3 className="my-2 text-center text-gray-900 italic">{role}</h3>
+      <p className="my-2 text-center text-gray-900">{description}</p>
+    </div>
+  );
+};
 
 const teamStyle = {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    marginTop: "5em"
-}
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  marginTop: "5em",
+};
 
 const Team = () => {
   return (
@@ -55,14 +49,23 @@ const Team = () => {
           </div>
 
           <div style={teamStyle} data-aos="fade-in" data-aos-delay="600">
-            <Member name="Sergej Sakac" role="Core developer & Founder" description={`Sergej is a member of the Polkadot Fellowship. He has been an
+            <Member
+              name="Sergej Sakac"
+              role="Core developer & Founder"
+              description={`Sergej is a member of the Polkadot Fellowship. He has been an
                 external core contributor on substrate and polkadot for more
                 than a year now. Sergej is also a recent Engineering alumni of
-                the Polkadot Blockchain Academy (PBA) held in Berkeley.`} image={sergej}/>
-            <Member name="Oliver Lim" role="Full-stack developer" description={`Oliver is a member of the Polkadot Fellowship. He has been an
-                external core contributor on substrate and polkadot for more
-                than a year now. Oliver is also a recent Engineering alumni of
-                the Polkadot Blockchain Academy (PBA) held in Berkeley.`} image={oliver}/>
+                the Polkadot Blockchain Academy (PBA) held in Berkeley.`}
+              image={sergej}
+            />
+            <Member
+              name="Oliver Lim"
+              role="Full-stack developer"
+              description={`Oliver is a full stack blockchain developer with 2 years of Rust & Substrate development experience. 
+              He was involved in 3 projects granted by Web3 Foundation - Imbue network, Fair squares and the Dotflow. 
+              He worked with Sergej on the Dotflow application.`}
+              image={oliver}
+            />
           </div>
         </div>
       </section>
