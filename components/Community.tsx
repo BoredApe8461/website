12beee9722241d906057eb7f52ff7a675b7b1f30
BoredApe8461 <letesinkelegesse@gmail.com>
2023-12-25 07:02:31 +0100

    fix links

--- components/Community.tsx
+++ components/Community.tsx
@@ -14,7 +14,7 @@ function Community() {
                         <div data-aos="fade-up" data-aos-duration="3000" className="px-5 py-10 border border-sky-500 border-opacity-20 rounded-lg">
                             <img className="h-10 mb-6 mx-auto" src="https://freelogopng.com/images/all_img/1690643640twitter-x-icon-png.png" alt="content" />
                             <h3 className="tracking-widest text-sky-500 text-base font-medium title-font text-center">X (Twitter)</h3>
-                            <a href="" target="_blank" rel="noopener noreferrer">
+                            <a href="https://twitter.com/RegionXLabs" target="_blank" rel="noopener noreferrer">
                                 <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Follow</button>
                             </a>
                         </div>
@@ -22,14 +22,14 @@ function Community() {
                             <img className="h-10 mb-6 mx-auto" src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" alt="content" />
                             <h3 className="tracking-widest text-sky-500 text-base font-medium title-font text-center">Discord</h3>
                             <a href="" target="_blank" rel="noopener noreferrer">
-                                <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Join</button>
+                                <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Soon</button>
                             </a>
                         </div>
                         <div data-aos="fade-up" data-aos-duration="3000" className="px-5 py-10 border border-sky-500 border-opacity-20 rounded-lg">
                             <img className="h-10 mb-6 mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1024px-Telegram_2019_Logo.svg.png" alt="content" />
                             <h3 className="tracking-widest text-sky-500 text-base font-medium title-font text-center">Telegram</h3>
                             <a href="" target="_blank" rel="noopener noreferrer">
-                                <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Join</button>
+                                <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Soon</button>
                             </a>
                         </div>
                         <div data-aos="fade-up" data-aos-duration="3000" className="px-5 py-10 border border-sky-500 border-opacity-20 rounded-lg">
@@ -38,8 +38,8 @@ function Community() {
                                     <path d="M34.9717 17.6062C34.9717 27.3295 27.1429 35.2117 17.4862 35.2117C7.82938 35.2117 0 27.3277 0 17.6062C0 7.88463 7.82879 0 17.4862 0C27.1435 0 34.9717 7.88285 34.9717 17.6062ZM54.154 17.6062C54.154 26.7586 50.2396 34.181 45.4109 34.181C40.5822 34.181 36.6678 26.7586 36.6678 17.6062C36.6678 8.45376 40.5816 1.03132 45.4103 1.03132C50.239 1.03132 54.1534 8.45139 54.1534 17.6062M62 17.6062C62 25.8045 60.6235 32.4546 58.925 32.4546C57.2266 32.4546 55.8507 25.8063 55.8507 17.6062C55.8507 9.40607 57.2272 2.75772 58.925 2.75772C60.6229 2.75772 62 9.40548 62 17.6062Z" fill="#ffffff"></path>
                                 </svg>
                             </div>
-                            <h3 className="tracking-widest text-sky-500 text-base font-medium title-font text-center">X (Twitter)</h3>
-                            <a href="" target="_blank" rel="noopener noreferrer">
+                            <h3 className="tracking-widest text-sky-500 text-base font-medium title-font text-center">Medium</h3>
+                            <a href="https://medium.com/@regionx" target="_blank" rel="noopener noreferrer">
                                 <button className="flex mx-auto mt-5 text-white bg-gray-900 transition duration-200 hover:bg-sky-500 border-0 py-2 px-8 focus:outline-none rounded">Follow</button>
                             </a>
                         </div>
