155203598a24e819a92f479e7e2946e602dd8b9d
BoredApe8461 <letesinkelegesse@gmail.com>
2024-04-13 14:28:19 +0200

    mobile fix

--- components/Coretime.tsx
+++ components/Coretime.tsx
@@ -6,7 +6,7 @@ function Coretime() {
             <div className='max-w-7xl mx-auto px-7 2xl:px-0'>
                 <h1 className='text-white text-3xl font-semibold text-center pt-20'>What is Coretime?</h1>
 
-                <div data-aos="fade-up" className="flex justify-between lg:flex-row bg-opacity-5 rounded-lg p-10 lg:p-20">
+                <div data-aos="fade-up" className="flex flex flex-col space-between lg:flex-row-reverse lg:flex-row bg-opacity-5 rounded-lg p-10 lg:p-20">
                     <div data-aos="fade-up" data-aos-duration="3000" className='w-full lg:w-[40%]'>
                         <img src="/core.png" alt="" className=' scale-75' />
                     </div>
@@ -15,7 +15,7 @@ function Coretime() {
                             Projects need to reserve Coretime on Polkadot's cores to access its secure and interoperable blockspace. We are offering the platform to purchase chunks of Coretime cheaply.
                         </p>
 
-                        <a href="https://wiki.polkadot.network/docs/polkadot-direction" target='_blank' className='mt-2'>
+                        <a href="https://wiki.polkadot.network/docs/polkadot-direction" className="text-center mt-2 flex md:justify-start justify-center" target='_blank'>
                             <button className='border-2 border-sky-500 hover:bg-sky-500 text-white transition duration-300  px-5 py-2 rounded-md w-[150px] text-sm'>
                                 Learn more
                             </button>
